65ea963857549393e99ace69df06a121d8c8fa11
MmmPeaches <39601026+MmmPeaches@users.noreply.github.com>
2019-02-11 22:53:45 -0500

    Update fixed.cc

--- fixed.cc
+++ fixed.cc
@@ -3,13 +3,13 @@
 #include <cmath>
 
 
-double deviation( int a[], int SIZE)
+double deviation( int a[], int SIZE)//SIZE is the number of elements in the list
 {
 	double sum;	
 
 	for(size_t i = 0; i < SIZE; i++)
 	{
-		sum += a[i];
+		sum += a[i];//we find the sum by adding together each element by running the loop till SIZE
 	} 
 	double mean = sum /= SIZE;
 	double stddev = 0;
@@ -20,7 +20,7 @@ double deviation( int a[], int SIZE)
 
 	stddev /= SIZE;
 	
-	if( stddev == 0)
+	if( stddev == 0)//here the stddev was being set to 0 with =, so I changed it too ==
 		std::cout << "Sigma is zero." << std::endl;
 
 	return sqrt(stddev);
