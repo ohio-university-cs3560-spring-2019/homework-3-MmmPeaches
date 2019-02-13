9a85e3d6a3f190b49eb38789038058cda9b92650
MmmPeaches <39601026+MmmPeaches@users.noreply.github.com>
2019-02-13 16:15:23 -0500

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,10 @@
+#here's my solution for home work 4 writen in python
+sum = 0
+
+for i in range(0, 10):
+        if i % 3 == 0: 
+           sum += i
+        elif i % 5 == 0:
+           sum += i
+
+print sum
