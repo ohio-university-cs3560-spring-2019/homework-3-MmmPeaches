7197a8c15c014f20f342c2881c78f87414acb9e5
MmmPeaches <39601026+MmmPeaches@users.noreply.github.com>
2019-03-18 19:22:23 -0400

    Add files via upload

---
+++ c_expressions.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+########################################
+#
+#   ANDRIY KALINICHENKO
+#   HOMEWORK 8.1
+#    CS3560 SOFTWARE TOOLS - MORNING CHAD
+#   OHIO UNIVERSITY
+#   3/18/2019
+#   MILKY WAY - EARTH
+#
+########################################
+
+puts "enter a valid c++ identifier"
+
+str = $stdin.gets#reads in a string from the user
+
+def reg_ex(str)
+    str =~ /[0-9a-zA-Z_]/
+end
+
+if !reg_ex(str)
+    puts "your input does not contain valid c++ characters for naming an identifier"
+else
+    puts "your input contians valid c++ characters for naming an identifier" 
+end
+
